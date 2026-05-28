a351ee6484c4d108d45c50cec9cf75c1d0120c17
FelipeBlossom <flor.felipe@academico.ifpb.edu.br>
2026-05-28 00:29:27 -0300

    Adiciona estrutura da atividade

--- app.py
+++ app.py
@@ -1,1 +1,9 @@
 from flask import flash, render_template, redirect
+from app import app
+
+@app.route('/')
+def raiz():
+    return render_template('filmes.html')
+
+if __name__ == '__main__':
+    app.run(debug=True, port=3001)
